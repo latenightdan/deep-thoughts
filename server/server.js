f98d916b0fbcba6f4af3e9df035404f6c794b6fb
daniel natale <dnatale667@gmail.com>
2022-01-12 18:22:08 -0500

    moved

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
+
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -32,6 +34,13 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirnem, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
